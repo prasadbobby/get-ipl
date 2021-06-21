ad78736dec15342e2cea968e557e9fe7bf7f8bb0
prasadbobby <prasadbobby057@gmail.com>
2021-06-21 21:10:50 +0530

    Space

--- sw.js
+++ sw.js
@@ -59,7 +59,7 @@ const staticAssets = [
 self.addEventListener('install', async e => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
-  return self.skipWaiting();
+  return self.skipWaiting(); 
 });
 
 self.addEventListener('activate', e => {
